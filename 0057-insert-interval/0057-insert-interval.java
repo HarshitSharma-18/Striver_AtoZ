393dd660419b90dd1484ae01f69ab95a805286df
HarshitSharma-18 <harshit.m.sharma18@gmail.com>
2026-07-25 23:31:11 +0530

    Time: 1 ms (98.1%), Space: 46.9 MB (75.38%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,26 +1,39 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> newIntervals = new ArrayList<>(Arrays.asList(intervals));
+        int n = intervals.length;
+        ArrayList<int[]> newIntervals = new ArrayList<>();
+
+        int i = 0;
+        while (i < n && intervals[i][0] < newInterval[0]) {
+            newIntervals.add(intervals[i]);
+            i++;
+        }
+
+        newIntervals.add(newInterval);
+        
+        while (i < n) {
+            newIntervals.add(intervals[i]);
+            i++;
+        }
         newIntervals.add(newInterval);
 
-        int n = newIntervals.size();
-        newIntervals.sort((a,b) -> Integer.compare(a[0] , b[0]));
+        n = newIntervals.size();
 
         ArrayList<int[]> result = new ArrayList<>();
         result.add(newIntervals.get(0));
 
-        for(int i = 1; i< n ; i++){
+        for(int j = 1; j< n ; j++){
             int s1 = result.get(result.size()-1)[0];
-            int s2 = newIntervals.get(i)[0];
+            int s2 = newIntervals.get(j)[0];
             int e1 = result.get(result.size()-1)[1];
-            int e2 = newIntervals.get(i)[1];
+            int e2 = newIntervals.get(j)[1];
 
             if(s2 <= e1){
                 result.get(result.size()-1)[0] = s1;
                 result.get(result.size()-1)[1] = Math.max(e1 , e2);
             }
             else{
-                result.add(newIntervals.get(i));
+                result.add(newIntervals.get(j));
             }
         }
 
