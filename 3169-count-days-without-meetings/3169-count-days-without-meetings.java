81f8ff4bc84e4a02e5beb06bf4460cfa944c412f
HarshitSharma-18 <harshit.m.sharma18@gmail.com>
2026-07-25 22:15:36 +0530

    Time: 40 ms (39.44%), Space: 123.8 MB (72.15%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.java
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.java
@@ -7,14 +7,15 @@ class Solution {
         result.add(meetings[0]);
 
         for(int i = 1 ; i<n ; i++){
-            int s1 = result.get(result.size() - 1)[0];
-            int e1 = result.get(result.size() - 1)[1];
+            int resultSize = result.size();
+            int s1 = result.get(resultSize - 1)[0];
+            int e1 = result.get(resultSize - 1)[1];
             int s2 = meetings[i][0];
             int e2 = meetings[i][1];
 
             if(s2 <= e1 && s1 <= e2){
-                result.get(result.size() - 1)[0] = Math.min(s1, s2);
-                result.get(result.size()-1)[1] = Math.max(e1 , e2);
+                result.get(resultSize - 1)[0] = Math.min(s1, s2);
+                result.get(resultSize-1)[1] = Math.max(e1 , e2);
             }
             else{
                 result.add(meetings[i]);
