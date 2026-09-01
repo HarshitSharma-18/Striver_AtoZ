2beb6fe1611778bba7311fd534a79f2e481e0a1b
HarshitSharma-18 <harshit.m.sharma18@gmail.com>
2026-09-01 15:17:10 +0530

    Time: 14 ms (70.6%), Space: 47.3 MB (96.12%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,48 +1,38 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-         // 1. Create frequency map
-    HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
 
-    for (int num : nums) {
-        map.put(num, map.getOrDefault(num, 0) + 1);
-    }
+        HashMap<Integer , Integer> map = new HashMap<>();
 
-    // 2. Convert map entries into a List
-    List<Map.Entry<Integer, Integer>> list =
-            new ArrayList<>(map.entrySet());
-
-    // 3. Min Heap based on frequency
-    PriorityQueue<Map.Entry<Integer, Integer>> minHeap =
-            new PriorityQueue<>(
-                    (a, b) -> Integer.compare(a.getValue(), b.getValue())
-            );
-
-    // 4. Put first k elements into heap
-    for (int i = 0; i < k; i++) {
-        minHeap.add(list.get(i));
-    }
-
-    // 5. Traverse remaining elements
-    for (int i = k; i < list.size(); i++) {
-
-        // Current element has higher frequency
-        // than the smallest frequency in heap
-        if (list.get(i).getValue() > minHeap.peek().getValue()) {
-
-            minHeap.poll();          // remove smallest frequency
-            minHeap.add(list.get(i)); // add new element
+        for(int i = 0; i < n ; i++){
+            map.put(nums[i] , map.getOrDefault(nums[i] , 0) + 1);
         }
-    }
 
-    // 6. Put heap elements into answer
-    int[] result = new int[k];
-    int i = 0;
+        List<Map.Entry<Integer , Integer>> list = new ArrayList<>(map.entrySet());
 
-    while (!minHeap.isEmpty()) {
-        result[i] = minHeap.poll().getKey();
-        i++;
-    }
+        PriorityQueue<Map.Entry<Integer , Integer>> minHeap = new PriorityQueue<>((a , b) -> Integer.compare(a.getValue() , b.getValue()));
 
-    return result;
+        for(Map.Entry<Integer , Integer> entries : list){
+            if(minHeap.size() == k) break;
+            minHeap.add(entries);
+        }
+
+        int[] arr = new int[k];
+
+        for(int i = k ; i < list.size() ; i++){
+            if(minHeap.peek().getValue() < list.get(i).getValue()){
+                minHeap.poll();
+                minHeap.add(list.get(i));
+            }
+        }
+
+        int i = 0;
+        while(!minHeap.isEmpty()){
+            arr[i] = minHeap.peek().getKey();
+            minHeap.poll();
+            i++;
+        }
+
+        return arr;        
     }
 }
