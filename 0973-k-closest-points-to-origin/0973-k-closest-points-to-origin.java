d5b1b62ea366a99061cd35ee48db40d71892a75e
HarshitSharma-18 <harshit.m.sharma18@gmail.com>
2026-09-02 10:21:08 +0530

    Time: 33 ms (30.44%), Space: 53.8 MB (73.25%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -2,29 +2,24 @@ class Solution {
     public int[][] kClosest(int[][] points, int k) {
         int n = points.length;
 
-        HashMap<Integer , Double> map = new HashMap<>();
+        List<Pair> list = new ArrayList<>();
 
         for(int i = 0 ; i < n ; i++){
-            double distance = distanceHelper(points[i][0] , points[i][1]);
-
-            map.put(i , distance);
+            list.add(new Pair(i , distanceHelper(points[i][0] , points[i][1])));
         }
 
-        PriorityQueue<Map.Entry<Integer , Double>> maxHeap = new PriorityQueue<>((a , b) -> Double.compare(b.getValue() , a.getValue()));
+        PriorityQueue<Pair> maxHeap = new PriorityQueue<>((a , b) -> Double.compare(b.distance , a.distance));
 
-        for(Map.Entry<Integer , Double> entries : map.entrySet()){
-            if(maxHeap.size() == k){
+        for(Pair entries : list){
+            if(maxHeap.size() == k) {
                 break;
             }
             maxHeap.add(entries);
-
         }
 
-        List<Map.Entry<Integer , Double>> list = new ArrayList<>(map.entrySet());
         int m = list.size();
-
         for(int i = k ; i < m ; i++){
-            if(maxHeap.peek().getValue() > list.get(i).getValue()){
+            if(maxHeap.peek().distance > list.get(i).distance){
                 maxHeap.poll();
                 maxHeap.add(list.get(i));
             }
@@ -34,7 +29,7 @@ class Solution {
 
         int i = 0;
         while(!maxHeap.isEmpty()){
-            int index = maxHeap.poll().getKey();
+            int index = maxHeap.poll().index;
             result[i] = points[index];
             i++;
         }
@@ -43,5 +38,15 @@ class Solution {
     }
     public double distanceHelper(double x1 , double y1){
         return Math.sqrt((x1*x1) + (y1*y1));
+    }
+}
+
+class Pair{
+    int index;
+    double distance;
+
+    Pair(int index , double distance){
+        this.index = index;
+        this.distance = distance;
     }
 }
