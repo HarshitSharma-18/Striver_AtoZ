759e79bf5d64d720b425305adc65a5750c1f2011
HarshitSharma-18 <harshit.m.sharma18@gmail.com>
2026-09-02 12:02:14 +0530

    Time: 42 ms (15.22%), Space: 54.2 MB (53.42%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,36 +1,37 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int n = points.length;
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+            (a, b) -> Long.compare(
+                distance(b),
+                distance(a)
+            )
+        );
 
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a , b) -> Double.compare(distanceHelper(b) , distanceHelper(a)));
+        for (int i = 0; i < points.length; i++) {
 
-        for(int[] entries : points){
-            if(maxHeap.size() == k) {
-                break;
-            }
-            maxHeap.add(entries);
-        }
+            maxHeap.add(points[i]);
 
-        for(int i = k ; i < n ; i++){
-            if(distanceHelper(maxHeap.peek()) > distanceHelper((points[i]))){
+            if (maxHeap.size() > k) {
                 maxHeap.poll();
-                maxHeap.add(points[i]);
             }
         }
 
         int[][] result = new int[k][2];
 
         int i = 0;
-        while(!maxHeap.isEmpty()){
+
+        while (!maxHeap.isEmpty()) {
             result[i] = maxHeap.poll();
             i++;
         }
 
         return result;
     }
-    public double distanceHelper(int[] arr){
-        int x1 = arr[0];
-        int y1 = arr[1];
-        return Math.sqrt((x1*x1) + (y1*y1));
+
+    public long distance(int[] point) {
+        long x = point[0];
+        long y = point[1];
+
+        return x * x + y * y;
     }
 }
