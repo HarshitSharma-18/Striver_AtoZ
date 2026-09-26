82e817eccad4110822234ae1ec494b0e8de8f4c2
HarshitSharma-18 <harshit.m.sharma18@gmail.com>
2026-09-26 12:35:22 +0530

    Time: 8 ms (36.57%), Space: 48.3 MB (5.96%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -6,40 +6,28 @@ class Solution {
             points[num] += num;
         }
 
-        int[][] dp = new int[2][10001];
+        int[] dp = new int[10001];
 
-        for (int i = 0; i < 2; i++) {
-            Arrays.fill(dp[i], -1);
-        }
+        Arrays.fill(dp, -1);
 
-        return helper(dp , points , 0 ,  0);
+        return helper(0,points, dp);
     }
-    public int helper(int[][] dp, int[] points, int i, int free){
+
+    public int helper(int i, int[] points, int[] dp) {
+
         if (i >= points.length) {
             return 0;
         }
 
-        if (dp[free][i] != -1) {
-        return dp[free][i];
+        if (dp[i] != -1) {
+            return dp[i];
         }
 
-        // current number ko choose nahi kar sakte
-        if (free == 0) {
+        int take = points[i] + helper(i + 2, points, dp);
+        int noTake = helper(i + 1, points, dp);
 
-            dp[free][i] = helper(dp, points, i + 1, 1);
+        dp[i] = Math.max(take, noTake);
 
-            return dp[free][i];
-        }
-
-        // current number choose kar sakte ho
-        int choice1 = points[i] + helper(dp, points, i + 1, 0);
-
-        // current number choose nahi karna
-        int choice2 = helper(dp, points, i + 1, 1);
-
-        dp[free][i] = Math.max(choice1, choice2);
-
-        return dp[free][i];
-
+        return dp[i];
     }
 }
